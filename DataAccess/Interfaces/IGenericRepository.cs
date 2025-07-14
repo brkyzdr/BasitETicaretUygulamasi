02c1d8cdcfe243946e1d39806db2a1b9a76aebf9
Berkay Ozder <berkayq3535@gmail.com>
2025-07-14 16:38:36 +0300

    Generic Repository Created

---
+++ DataAccess/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DataAccess.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        List<T> Find(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        void Save();
+    }
+}
---
+++ DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using DataAccess.Interfaces;
+
+namespace DataAccess.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public List<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public List<T> Find(Expression<Func<T, bool>> filter)
+        {
+            return _dbSet.Where(filter).ToList();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
