d0caba34f616d8fb0e074101f4d62d2fdc43c7b1
Berkay Ozder <berkayq3535@gmail.com>
2025-07-14 13:43:13 +0300

    N-tier Architecture Set

---
+++ DataAccess/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Entities;
+using System.Data.Entity;
+
+namespace DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        // Yapıcı metod ile bağlantı cümlesi (web.config içindeki "name=...")
+        public AppDbContext() : base("name=AppDbContext")
+        {
+        }
+
+        // DbSet tanımları
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        // Fluent API ayarları (gerekirse)
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Örnek: İlişki ayarı
+            modelBuilder.Entity<Product>()
+                .HasRequired(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId);
+        }
+    }
+}
