1cf9fc43a23d73828625b7930abe7bcc16abff43
Berkay Ozder <berkayq3535@gmail.com>
2025-07-16 16:28:59 +0300

    BugFix

--- BasitETicaretUygulamasi/App_Start/UnityConfig.cs
+++ BasitETicaretUygulamasi/App_Start/UnityConfig.cs
@@ -3,20 +3,31 @@ using Business.Services;
 using DataAccess;
 using DataAccess.Interfaces;
 using DataAccess.Repositories;
+using Entities;
+using System;
 using System.Web.Mvc;
 using Unity;
 using Unity.AspNet.Mvc;
-using Entities;
 
-namespace BasitETicaretUygulamasi.App_Start
+namespace BasitETicaretUygulamasi
 {
     public static class UnityConfig
     {
-        public static void RegisterComponents()
-        {
-            var container = new UnityContainer();
+        #region Unity Container
+        private static Lazy<IUnityContainer> container =
+            new Lazy<IUnityContainer>(() =>
+            {
+                var container = new UnityContainer();
+                RegisterTypes(container);
+                return container;
+            });
 
-            // Context
+        public static IUnityContainer Container => container.Value;
+        #endregion
+
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            // DbContext
             container.RegisterType<AppDbContext, AppDbContext>();
 
             // Generic Repository
@@ -25,9 +36,11 @@ namespace BasitETicaretUygulamasi.App_Start
             // Services
             container.RegisterType<IProductService, ProductService>();
             container.RegisterType<ICategoryService, CategoryService>();
+        }
 
-            // MVC Resolver
-            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        public static void RegisterComponents()
+        {
+            DependencyResolver.SetResolver(new UnityDependencyResolver(Container));
         }
     }
 }
