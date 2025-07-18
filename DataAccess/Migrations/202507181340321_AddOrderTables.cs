346c4f1100569da1779260613c8c8fccbde2f4d0
Berkay Ozder <berkayq3535@gmail.com>
2025-07-18 16:41:50 +0300

    Entities and DbContext Updated
    Orders can be save.

---
+++ DataAccess/Migrations/202507181340321_AddOrderTables.cs
@@ -0,0 +1,48 @@
+﻿namespace DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddOrderTables : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.OrderItems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ProductId = c.Int(nullable: false),
+                        OrderId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        UnitPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Orders", t => t.OrderId, cascadeDelete: true)
+                .ForeignKey("dbo.Products", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.ProductId)
+                .Index(t => t.OrderId);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        OrderDate = c.DateTime(nullable: false),
+                        TotalPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.OrderItems", "ProductId", "dbo.Products");
+            DropForeignKey("dbo.OrderItems", "OrderId", "dbo.Orders");
+            DropIndex("dbo.OrderItems", new[] { "OrderId" });
+            DropIndex("dbo.OrderItems", new[] { "ProductId" });
+            DropTable("dbo.Orders");
+            DropTable("dbo.OrderItems");
+        }
+    }
+}
---
+++ Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
